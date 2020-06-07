385438b3ca7da6e1dc1bd27a3226063b37682123
learnjs2020 <54968697+learnjs2020@users.noreply.github.com>
2020-06-07 16:38:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,56 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+var ECT = require('ect');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const port = 3000;
+var env = {
+    root : __dirname,
+    dataFolder : '/var/_localData',
+    appFolder : '/var/_localApp'
+}
+var pkg = {
+    crowdProcess : require(__dirname + '/vendor/crowdProcess/crowdProcess.js'),
+    tpl : ECT({ watch: true, cache: false, root: __dirname + '/views', ext : '.ect' })
+}
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.engine('ect', pkg.tpl.render);
+
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies   
+  extended: true
+})); 
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.get(/(.+)$/i, (req, res) => {
+    try {
+        delete require.cache[__dirname + '/modules/appRouter.js'];
+        var APP = require(__dirname + '/modules/appRouter.js');
+        var app = new APP(env, pkg, req, res);
+        app.get();
+    } catch (err) {
+        res.send(err.toString());
+    }
+
+});
+
+app.post(/(.+)$/i, (req, res) => {
+    try {
+        delete require.cache[__dirname + '/modules/appRouter.js'];
+        var APP = require(__dirname + '/modules/appRouter.js');
+        var app = new APP(env, pkg, req, res);
+        app.post();
+    } catch (err) {
+        res.send(err.toString());
+    }
+
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
